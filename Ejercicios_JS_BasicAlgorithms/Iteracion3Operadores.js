9c153242a5a7800b6c3e73e1f3314a782bee940e
Alba Puente Díaz <albapuentediaz@MacBook-Pro-de-Alba.local>
2021-12-08 16:59:04 +0100

    modificación Iteración#3-añado html

--- Ejercicios_JS_BasicAlgorithms/Iteracion3Operadores.js
+++ Ejercicios_JS_BasicAlgorithms/Iteracion3Operadores.js
@@ -1,21 +1,21 @@
 // 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
 let a = 10;
 let b = 5;
-console.error(a*b);
+alert(a*b);
 
 // // 1.2 Divide 10 por 2 y muestra el resultado en un alert.
-// let c = a / 2;
-console.error (a/2);
+let c = a / 2;
+alert(c);
 
 // // 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
-// let e = 15 / 9;
-console.error ((a+b)/9);
+let e = 15 / 9;
+alert(e);
 
 // // 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
 // // teniendo dos variables y = 10 y z = 5.
 let y = 10;
 let z = 5;
-// let x = y + z;
+let x = y + z;
 console.log (x);
 
 // // 1.5 Usa el correcto operador de asignación que resultará en x = 50,
