35d8832ac0b098eeabc48b225d2778255e55013e
Alba Puente Díaz <albapuentediaz@MacBook-Pro-de-Alba.local>
2021-12-08 16:32:10 +0100

    Iteracion #1

---
+++ Ejercicios_JS_BasicAlgorithms/Iteracion1Variables.js
@@ -0,0 +1,14 @@
+
+// Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+// Crea una variable llamada x, asigna el valor 50 a ella.
+// Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+// Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+
+let myFavoriteHero = "Hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+
+console.log(z);
+
